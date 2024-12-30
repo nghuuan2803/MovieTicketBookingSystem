29fb1cd80ae43ab381e3dc1483c0279103002c36
John Doe <nghuuan2803@gmail.com>
2024-12-30 09:54:43 +0700

    Task-10 Completed

--- MTBS.Domain/Entities/Movie.cs
+++ MTBS.Domain/Entities/Movie.cs
@@ -9,5 +9,13 @@ namespace MTBS.Domain.Entities
 {
     public class Movie : BaseEntity<Guid>
     {
+        public required string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public string? Studio { get; set; } = string.Empty;
+        public string? Director { get; set; } = string.Empty;
+        public string? Actors { get; set; } = string.Empty; //Json
+        public DateTimeOffset ReleaseDate { get; set; }
+        public int Duration { get; set; }
+        public bool IsOpen { get; set; }
     }
 }
